0a4a3d26d75690335eb4783a33ad1a3af03284a8
kyle <kyle201817146@gmail.com>
2021-05-31 12:10:02 +0800

    Package the top level function into a class and refactor test method

--- Sources/PacketProtocol/PacketProtocol.swift
+++ Sources/PacketProtocol/PacketProtocol.swift
@@ -1,5 +1,67 @@
 import Foundation
 
+public class PacketProtocol {
+    public static func process(at path: String,
+                               sensorCompletion: @escaping (SensorPacketHeader, [Data]) -> Void = { _,_  in },
+                               physicalCompletion: @escaping (PhysicalPacketHeader, [Data]) -> Void = { _,_  in }) -> Bool {
+        guard let handle = FileHandle(forReadingAtPath: path) else {
+            fatalError()
+        }
+
+        let total = handle.availableData.count
+        try! handle.seek(toOffset: 0)
+        while try! handle.offset() != total {
+            guard let blockTypeData = try? handle.peek(upToCount: MemoryLayout<BlockType>.stride) else {
+                print("DATA READ ERROR")
+                return false
+            }
+            let blockType: BlockType = blockTypeData.asStruct()
+            switch blockType {
+            case BLOCK_TYPE_SENSOR:
+                parseSensorPacket(handle, completion: sensorCompletion)
+            case BLOCK_TYPE_PHYSICAL:
+                parsePhysicalPacket(handle, completion: physicalCompletion)
+            default:
+                print("UNKNOWN BLOCK TYPE")
+                return false
+            }
+        }
+        return true
+    }
+
+    private static func parseSensorPacket(_ handle: FileHandle, completion: @escaping (SensorPacketHeader, [Data]) -> Void) {
+        guard let headerData = try? handle.read(upToCount: MemoryLayout<SensorPacketHeader>.stride) else {
+            print("DATA READ ERROR")
+            return
+        }
+        let header: SensorPacketHeader = headerData.asStruct()
+        let sampleData = parseData(handle, header: header)
+        completion(header, sampleData)
+    }
+
+    private static func parsePhysicalPacket(_ handle: FileHandle, completion: @escaping (PhysicalPacketHeader, [Data]) -> Void) {
+        guard let headerData = try! handle.read(upToCount: MemoryLayout<PhysicalPacketHeader>.stride) else {
+            print("DATA READ ERROR")
+            return
+        }
+        let header: PhysicalPacketHeader = headerData.asStruct()
+        let sampleData = parseData(handle, header: header)
+        completion(header, sampleData)
+    }
+
+    private static func parseData(_ handle: FileHandle, header: SensorPacketHeader) -> [Data] {
+        let dataSize: Int = header.unitNumber * header.dataUnit.size
+        let number = Int(header.dataLength) / dataSize
+        return (0 ..< number).compactMap { _ in try? handle.read(upToCount: dataSize) }
+    }
+
+    private static func parseData(_ handle: FileHandle, header: PhysicalPacketHeader) -> [Data] {
+        let dataSize: Int = 1
+        let number = Int(header.dataLength) / dataSize
+        return (0 ..< number).compactMap { _ in try? handle.read(upToCount: dataSize) }
+    }
+}
+
 extension Data {
     func asStruct<T>(fromByteOffset offset: Int = 0) -> T {
         return withUnsafeBytes { $0.load(fromByteOffset: offset, as: T.self) }
@@ -14,63 +76,4 @@ extension FileHandle {
         try seek(toOffset: originalOffset)
         return data
     }
-}
-
-func parseSensorPacket(_ handle: FileHandle) {
-    guard let headerData = try? handle.read(upToCount: MemoryLayout<SensorPacketHeader>.stride) else {
-        print("DATA READ ERROR")
-        return
-    }
-    let header: SensorPacketHeader = headerData.asStruct()
-    let sampleData = parseData(handle, header: header)
-    print(header)
-    print(sampleData)
-}
-
-func parsePhysicalPacket(_ handle: FileHandle) {
-    guard let headerData = try! handle.read(upToCount: MemoryLayout<PhysicalPacketHeader>.stride) else {
-        print("DATA READ ERROR")
-        return
-    }
-    let header: PhysicalPacketHeader = headerData.asStruct()
-    let sampleData = parseData(handle, header: header)
-    print(header)
-    print(sampleData)
-}
-
-func parseData(_ handle: FileHandle, header: SensorPacketHeader) -> [Data] {
-    let dataSize: Int = header.unitNumber * header.dataUnit.size
-    let number = Int(header.dataLength) / dataSize
-    return (0 ..< number).compactMap{ _ in try? handle.read(upToCount: dataSize)}}
-
-func parseData(_ handle: FileHandle, header: PhysicalPacketHeader) -> [Data] {
-    let dataSize: Int = 1
-    let number = Int(header.dataLength) / dataSize
-    return (0 ..< number).compactMap{ _ in try? handle.read(upToCount: dataSize)}
-}
-
-func process(at path: String) -> Bool{
-    guard let handle = FileHandle(forReadingAtPath: path) else {
-        fatalError()
-    }
-
-    let total = handle.availableData.count
-    try! handle.seek(toOffset: 0)
-    while try!handle.offset() != total {
-        guard let blockTypeData = try? handle.peek(upToCount: MemoryLayout<BlockType>.stride) else {
-            print("DATA READ ERROR")
-            return false
-        }
-        let blockType: BlockType = blockTypeData.asStruct()
-        switch blockType {
-        case BLOCK_TYPE_SENSOR:
-            parseSensorPacket(handle)
-        case BLOCK_TYPE_PHYSICAL:
-            parsePhysicalPacket(handle)
-        default:
-            print("UNKNOWN BLOCK TYPE")
-            return false
-        }
-    }
-    return true
 }
--- Tests/PacketProtocolTests/PacketProtocolTests.swift
+++ Tests/PacketProtocolTests/PacketProtocolTests.swift
@@ -2,10 +2,19 @@
 import XCTest
 
 final class PacketProtocolTests: XCTestCase {
-
     func testData() {
         let path = Bundle.module.path(forResource: "reference_data_v1", ofType: "bin", inDirectory: "TestData") ?? ""
-        let result = process(at: path)
+        let result = PacketProtocol.process(at: path) { header, samplesData in
+            print(header)
+            print(samplesData.map { data -> (UInt16, UInt16) in
+                (data.asStruct(),data.asStruct(fromByteOffset: 2))
+            })
+        } physicalCompletion: { header, samplesData in
+            print(header)
+            print(samplesData.map { data -> UInt8 in
+                data.asStruct()
+            })
+        }
         XCTAssertTrue(result)
     }
 }
