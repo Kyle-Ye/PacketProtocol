82daa0ef56ac59d0fe74c402fe9445f5fee19766
Kyle <kyle201817146@gmail.com>
2022-09-29 23:20:15 +0800

    Update manifest and add script

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,21 +11,15 @@ let package = Package(
         .macOS(.v11)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PacketProtocol",
             targets: ["PacketProtocol"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "PacketProtocol",
-            dependencies: []),
+        .target(name: "PacketProtocol"),
         .testTarget(
             name: "PacketProtocolTests",
             dependencies: ["PacketProtocol"],
